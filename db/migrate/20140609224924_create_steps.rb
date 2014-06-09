40bf2948c8aef49e084be2cb0c94b9821c004656
Greg Meyer <gregory.matthew.meyer@gmail.com>
2014-06-09 19:13:34 -0400

    adds a comment

--- db/migrate/20140609224924_create_steps.rb
+++ db/migrate/20140609224924_create_steps.rb
@@ -1,6 +1,7 @@
 class CreateSteps < ActiveRecord::Migration
   def change
     create_table :steps do |t|
+      # the text is just a placeholder for the moment.
       t.text :content
 
       t.timestamps
