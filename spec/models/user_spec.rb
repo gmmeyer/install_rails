eb44bcc11999064477816316c48ee687376262e3
Greg Meyer <gregory.matthew.meyer@gmail.com>
2014-06-15 01:58:24 -0400

    adds admin users, tests all pass

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,38 +24,37 @@ describe User do
   describe "Password Validations" do
     context "on a new user" do
       it "should not be valid without a password" do
-        user = User.new username: 'testing',
-          password: nil, password_confirmation: nil
-        user.should_not be_valid
+        user = User.new(username: 'testing', password: nil, password_confirmation: nil)
+        expect(user).to_not be_valid
       end
 
       it "should be not be valid with a short password" do
-        user = User.new username: 'testing',
-          password: 'short', password_confirmation: 'short'
-        user.should_not be_valid
+        user = User.new(username: 'testing', password: 'short', password_confirmation: 'short')
+        expect(user).to_not be_valid
       end
 
       it "should not be valid with a confirmation mismatch" do
-        user = User.new username: 'testing',
-          password: 'short', password_confirmation: 'long'
-        user.should_not be_valid
+        user = User.new(username: 'testing', password: 'short', password_confirmation: 'long')
+        expect(user).to_not be_valid
       end
     end
 
     context "on an existing user" do
       let(:user) do
-        u = User.create username: 'testing',
-          password: 'password', password_confirmation: 'password'
-        User.find u.id
+        User.find_by(username: 'testing')
+      end
+
+      before(:example) do
+        User.create(username: 'testing', password: 'password', password_confirmation: 'password')
       end
 
       it "should be valid with no changes" do
-        user.should be_valid
+        expect(user).to be_valid
       end
 
       it "should be valid with a new (valid) password" do
-        user.password = user.password_confirmation = "new password"
-        user.should be_valid
+        user.password = user.password_confirmation = "newpassword"
+        expect(user).to be_valid
       end
     end
   end
