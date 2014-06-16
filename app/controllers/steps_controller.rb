a847cd174cfe3470bd8eee2a83742a71af3a80e1
Greg Meyer <gregory.matthew.meyer@gmail.com>
2014-06-16 18:17:42 -0400

    fixes typo

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -28,45 +28,43 @@ class StepsController < ApplicationController
       @other = @step.next_edges.includes(:next_step).find_by(button_text: "Other")
     end
 
+    render :show
   end
 
-  render :show
-end
-
-def edit
-  @step = Step.find_by(permalink: params[:id])
-  unauthorized! if cannot? :manage, @step
-end
-
-def create
-  @step = Step.new(step_params)
-  unauthorized! if cannot? :manage, @step
-  if @step.save
-    redirect_to #admin thing
-  else
-    flash[:errors] = @step.errors.full_messages
+  def edit
+    @step = Step.find_by(permalink: params[:id])
+    unauthorized! if cannot? :manage, @step
   end
-end
 
-def update
-  @step = Step.find_by(permalink: params[:id])
-  unauthorized! if cannot? :manage, @step
-  if @step.save
+  def create
+    @step = Step.new(step_params)
+    unauthorized! if cannot? :manage, @step
+    if @step.save
+      redirect_to #admin thing
+    else
+      flash[:errors] = @step.errors.full_messages
+    end
+  end
+
+  def update
+    @step = Step.find_by(permalink: params[:id])
+    unauthorized! if cannot? :manage, @step
+    if @step.save
+      redirect_to #admin page
+    else
+      flash[:errors] = @step.errors.full_messages
+      redirect_to #admin page
+    end
+  end
+
+  def delete
+    @step = Step.find(permalink: params[:id])
+    @step.destroy
     redirect_to #admin page
-  else
-    flash[:errors] = @step.errors.full_messages
-    redirect_to #admin page
   end
-end
 
-def delete
-  @step = Step.find(permalink: params[:id])
-  @step.destroy
-  redirect_to #admin page
-end
-
-private
-def step_params
-  params.require(:step).permit(:title, :content, :trouble, :mixpanel, :final_step, :first_step, :prompt, :permalink)
-end
+  private
+  def step_params
+    params.require(:step).permit(:title, :content, :trouble, :mixpanel, :final_step, :first_step, :prompt, :permalink)
+  end
 end
